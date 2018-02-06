32b408f782665c34790f5bcd4a85e5bcaea5b8a8
Giang Tran <giang.tranthicamgiang@gmail.com>
2018-02-06 08:59:19 +0700

    Initial Commit

---
+++ TableViewTest3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewTest3
+//
+//  Created by Tran Thi Cam Giang on 2/6/18.
+//  Copyright © 2018 Tran Thi Cam Giang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
